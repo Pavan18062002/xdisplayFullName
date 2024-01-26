006c2b09401894e28cafe226ee3685812a230709
Pavan18062002 <pavansai0022@gmail.com>
2024-01-26 07:55:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [formData, setFormData] = useState({
+    firstname: '',
+    lastname: '',
+  });
+
+  const [fullName, setFullName] = useState('');
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const fullNameResult = `${formData.firstname} ${formData.lastname}`;
+    setFullName(fullNameResult);
+    console.log('Form submitted:', formData);
+  };
+  const formStyle = {
+    fontFamily: 'Times New Roman, serif',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={formStyle}>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          First Name:
+          <input
+            type="text"
+            name="firstname"
+            value={formData.firstname}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          Last Name:
+          <input
+            type="text"
+            name="lastname"
+            value={formData.lastname}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button style={{ marginTop: '5px' }} type="submit">
+          Submit
+        </button>
+      </form>
+
+      {fullName && (
+        <div>
+          <p>Full Name: {fullName}</p>
+        </div>
+      )}
     </div>
   );
 }
